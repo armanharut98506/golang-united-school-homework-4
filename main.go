880b2cc03f4c0327c5b790b4fb1a96324525f328
armanharut98506 <armanharut7117@gmail.com>
2022-06-27 01:57:50 +0400

    Solution for task4.2

--- main.go
+++ main.go
@@ -1,29 +1,19 @@
-package string_sum
+package main
 
 import (
-	"errors"
-	"regexp"
+	"fmt"
 	"strconv"
-)
+	"regexp"
+	"errors"
+) 
 
-//use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
 	// Use when the input is empty, and input is considered empty if the string contains only whitespace
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
-	errorNotValidInput = errors.New("input not valid")
+	errorNotValidInput = errors.New("error is not valid")
 )
-
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
 	if input == "" {
@@ -46,4 +36,9 @@ func StringSum(input string) (output string, err error) {
 	num2, _ := strconv.Atoi(nums[1])
 
 	return strconv.Itoa(num1 + num2), nil
+}
+
+func main() {
+	input := " -8  -5 "
+	fmt.Println(StringSum(input))
 }
